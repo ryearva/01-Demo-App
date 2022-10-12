a9e368164e273545e661907a4544f469ebe6e4e9
ryearva <rramanareddy28@gmail.com>
2022-10-12 23:00:42 +0530

    first commmit

---
+++ Demo.java
@@ -0,0 +1,21 @@
+public class Tarak {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello Ramana");
+		int x = 0;
+		if (x == 0) {
+			System.out.println("I am IF Block");
+		} else {
+			System.out.println("i am Else Block");
+		
+		}
+	}
+				System.out.println("I out side loop");
+
+	public void m1()
+	{
+		System.out.println("Added new lly");
+		}
+
+}
